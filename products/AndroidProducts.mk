3225bb19b765bf930b6a4dab2acc9512f6a97877
CuriousNom <aryanbiswas7585@gmail.com>
2026-03-09 16:19:52 +0530

    Revert "products: Introduce Nothing Phone 3a (asteroids)"
    This reverts commit 49ec349a71e1d53df09e98bdccd39b216a9b5228.

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -7,7 +7,6 @@
 PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/alioth/aospa_alioth.mk \
     $(LOCAL_DIR)/apollo/aospa_apollo.mk \
-    $(LOCAL_DIR)/asteroids/aospa_asteroids.mk \
     $(LOCAL_DIR)/bitra/aospa_bitra.mk \
     $(LOCAL_DIR)/bladerunner/aospa_bladerunner.mk \
     $(LOCAL_DIR)/davinci/aospa_davinci.mk \
@@ -42,7 +41,6 @@ PRODUCT_MAKEFILES += \
 COMMON_LUNCH_CHOICES += \
     aospa_alioth-userdebug \
     aospa_apollo-userdebug \
-    aospa_asteroids-userdebug \
     aospa_bitra-userdebug \
     aospa_bladerunner-userdebug \
     aospa_davinci-userdebug \
